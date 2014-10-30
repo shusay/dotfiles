f14d07bb5acb841ee55368d7c46feeb573e0a6a7
Shusei Tanaka <shusay.t@gmail.com>
2014-10-30 09:02:47 +0900

    gvimrc追加

---
+++ .gvimrc
@@ -0,0 +1,7 @@
+" font setting
+set guifont=Ricty Regular for Powerline:h16
+set guifontwide=Ricty Regular for Powerline:h16
+
+set antialias
+
+colorscheme Solarized
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zshrc .zprofile .bashrc .bash_profile .tmux.conf .tmux .vimrc .gitconfig .gitignore .zshrc.antigen )
+DOT_FILES=( .zshrc .zprofile .bashrc .bash_profile .tmux.conf .tmux .vimrc .gvimrc .gitconfig .gitignore .zshrc.antigen )
 
 for file in ${DOT_FILES[@]}
 do
